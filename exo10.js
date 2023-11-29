80b4d1b8844f3d76d5344898d915e451f35f6006
AbdiLouay <ablouay5@gmail.com>
2023-11-29 11:05:47 +0100

    4

---
+++ exo10.js
@@ -0,0 +1,27 @@
+
+
+var compteur = 0;
+var compteur2 = 0;
+
+var Madiv = document.getElementById("Methode2");
+Madiv.addEventListener("click", UneProcedureQuiChangeLetext);
+
+Madiv = document.getElementById("Methode3");
+Madiv.addEventListener("mouseover", UneProcedureQuiSurvole);
+
+Madiv = document.getElementById("Methode4");
+Madiv.addEventListener("keydown", UneProcedureQuiEcrit);
+
+function UneProcedureQuiEcrit(evenment) {
+evenement.target.innerHTML = "On a appuyé sur : "+evenment.key;
+}
+
+function UneProcedureQuiChangeLetext(evenment) {
+    compteur++;
+    evenement.target.innerHTML = "on m'a cliqué dessus "+compteur+" fois";
+    }
+
+    function UneProcedureQuiSurvole(evenment) {
+        compteur2++;
+        evenement.target.innerHTML = "on m'a survollé "+compteur2+" fois";
+    }
